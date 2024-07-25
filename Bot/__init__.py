db45554b59c4a22ec78500174a2c96ad9ada7af5
LynetteTheCat <127607715+silly-moon@users.noreply.github.com>
2024-07-25 20:14:43 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -16,7 +16,7 @@ API_ID = int(os.environ.get('API_ID', 27421338)) #Telgram Api id
 APP_HASH = os.environ.get('APP_HASH', 'eca0538991683e182a7cfbbbc4aa3928')# Telgram App hash  
 OWNER_ID = int(os.environ.get('OWNER_ID', 5505492758))
 MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://lynette47:lynette47@cluster0.0r4hpe3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002213785570)    # Must Fill This ,Add Bot As Admin In Log Channel
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002182396169)    # Must Fill This ,Add Bot As Admin In Log Channel
 BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
 #<-----------Variables For 4GB Support (Optional)-------------->
 SESSION_STRING = os.environ.get("SESSION_STRING",'None')  #Replace None With String Session   Your String Session Account Must Be Present In Log Channel
